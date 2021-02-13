f695ca7b16fea6c1cc5d1ae90d65dfc4b55926f9
vilsonmoro <vilsonmoro@gmail.com>
2021-02-13 07:38:03 -0300

    Correção visibilidade do perfil

--- src/pages/Home/Search/index.tsx
+++ src/pages/Home/Search/index.tsx
@@ -11,6 +11,8 @@ const Search = () => {
     const [perfil, setPerfil] = useState<Perfil>();
     const [path, setPath] = useState('');
     const [isLoading, setIsloading] = useState(false);
+    const [visiblePerfil, setVisiblePerfil] = useState(false);
+
 
     const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setPath(event.target.value);
@@ -23,13 +25,13 @@ const Search = () => {
         }
         //pesquisa 
         setIsloading(true);
-
         fetch('https://api.github.com/users/' + payload.path)
-            .then(response => response.json())
+            .then(response => {
+                response.ok ? setVisiblePerfil(true) : setVisiblePerfil(false);
+                return response.json();
+            })
             .then(data => setPerfil(data))
-            .finally(() => {
-                setIsloading(false);
-            });
+            .finally(() => setIsloading(false));
 
     }
 
@@ -55,7 +57,7 @@ const Search = () => {
                 <div className="search-result-container">
                     {
                         isLoading ? <ImageLoader /> : (
-                            <div className="search-result-col-3">
+                            <div className={`${visiblePerfil ? "search-result-col-3-v" : "search-result-col-3"}`}>
                                 <img src={perfil?.avatar_url} alt={perfil?.avatar_url} className="search-img" />
 
                                 <a href={perfil?.html_url} >
@@ -66,27 +68,28 @@ const Search = () => {
                     }
 
                     {
-                        isLoading ? <InfoLoader /> : (<div className="search-result-col-7">
-                            <div className="search-item-info">
-                                <span className="search-info-data">
-                                    Repositorios publicos:{perfil?.public_repos}
-                                </span>
-                                <span className="search-info-data">
-                                    Seguidores: {perfil?.followers}
-                                </span>
-                                <span className="search-info-data">
-                                    Seguidos: {perfil?.following}
-                                </span>
-                            </div>
+                        isLoading ? <InfoLoader /> : (
+                            <div className={`${visiblePerfil ? "search-result-col-7-v" : "search-result-col-7"}`}>
+                                <div className="search-item-info">
+                                    <span className="search-info-data">
+                                        Repositorios publicos:{perfil?.public_repos}
+                                    </span>
+                                    <span className="search-info-data">
+                                        Seguidores: {perfil?.followers}
+                                    </span>
+                                    <span className="search-info-data">
+                                        Seguidos: {perfil?.following}
+                                    </span>
+                                </div>
 
-                            <div className="search-result-informacao">
-                                <p> Informações</p>
-                                <p><span>Empresa:</span> {perfil?.company} </p>
-                                <p><span>Website/Blog:</span> {perfil?.blog}</p>
-                                <p><span>Localidade:</span> {perfil?.location}</p>
-                                <p><span>Membro desde: </span>{perfil?.created_at}</p>
-                            </div>
-                        </div>)
+                                <div className="search-result-informacao">
+                                    <p> Informações</p>
+                                    <p><span>Empresa:</span> {perfil?.company} </p>
+                                    <p><span>Website/Blog:</span> {perfil?.blog}</p>
+                                    <p><span>Localidade:</span> {perfil?.location}</p>
+                                    <p><span>Membro desde: </span>{perfil?.created_at}</p>
+                                </div>
+                            </div>)
                     }
                 </div>
             </div>
